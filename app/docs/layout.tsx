31b9cad9131e41a7e382c791cf281f987543589a
simon <99.cent.bagel@gmail.com>
2026-01-08 15:01:49 -0800

    Remove duplicate dynamic export in docs layout

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -3,7 +3,6 @@ import type { ReactNode } from "react";
 import { getSource } from "@/lib/source";
 import { baseOptions } from "@/app/docs/base-options";
 import { Body } from "./layout.client";
-import { BoxIcon } from "lucide-react";
 
 export default async function Layout({ children }: { children: ReactNode }) {
   const source = await getSource();
@@ -14,28 +13,6 @@ export default async function Layout({ children }: { children: ReactNode }) {
         {...baseOptions}
         sidebar={{
           prefetch: false,
-          tabs: [
-            {
-              title: "Bagel Docs",
-              description: "documentation systm",
-              icon: (
-                <span className="border border-blue-600/50 bg-gradient-to-t from-blue-600/30 rounded-lg p-1 text-blue-600">
-                  <BoxIcon />
-                </span>
-              ),
-              url: "/docs/pages",
-            },
-            {
-              title: "Page 2",
-              description: "Features available in /pages",
-              icon: (
-                <span className="border purple-blue-600/50 bg-gradient-to-t from-purple-600/30 rounded-lg p-1 text-purple-600">
-                  <BoxIcon />
-                </span>
-              ),
-              url: "/docs/pages",
-            },
-          ],
         }}
       >
         {children}
