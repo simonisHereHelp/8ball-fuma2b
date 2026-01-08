a4805eb2fa9cb7110ed9cbc247a490d237149a61
unknown <simon@ishere.help>
2026-01-08 11:18:57 -0800

    pr 13

--- lib/source.ts
+++ lib/source.ts
@@ -1,6 +1,6 @@
 import { loader } from "fumadocs-core/source";
 import * as path from "node:path";
-import { createGitHubSource } from "./sources/github";
+import { createDriveSource } from "./sources/drive";
 import { createLocalSource } from "./sources/local";
 
 const FileNameRegex = /^\d\d-(.+)$/;
@@ -8,26 +8,28 @@ const FileNameRegex = /^\d\d-(.+)$/;
 export const isLocal =
   process.env.LOCAL || process.env.NEXT_PHASE === "phase-production-build";
 
-export const source = loader({
-  baseUrl: "/docs",
-  source: isLocal ? await createLocalSource() : await createGitHubSource(),
-  slugs(info) {
-    const segments = info.flattenedPath
-      .split("/")
-      .filter((seg) => !(seg.startsWith("(") && seg.endsWith(")")))
-      .map((seg) => {
-        const res = FileNameRegex.exec(seg);
+export async function getSource() {
+  return loader({
+    baseUrl: "/docs/pages",
+    source: isLocal ? await createLocalSource() : await createDriveSource(),
+    slugs(info) {
+      const segments = info.flattenedPath
+        .split("/")
+        .filter((seg) => !(seg.startsWith("(") && seg.endsWith(")")))
+        .map((seg) => {
+          const res = FileNameRegex.exec(seg);
 
-        return res ? res[1] : seg;
-      });
+          return res ? res[1] : seg;
+        });
 
-    if (segments.at(-1) === "index") {
-      segments.pop();
-    }
+      if (segments.at(-1) === "index") {
+        segments.pop();
+      }
 
-    return segments;
-  },
-});
+      return segments;
+    },
+  });
+}
 
 export function getTitleFromFile(file: string) {
   const acronyms = ["css", "ui", "cli"];
